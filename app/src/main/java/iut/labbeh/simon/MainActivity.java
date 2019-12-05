8e3b2fc89c7a24e7441fe606645068176a28bca0
hugo labbé <hugo.labbe@etu.univ-lehavre.fr>
2019-12-05 16:02:40 +0100

    modification sur les sons

--- app/src/main/java/iut/labbeh/simon/MainActivity.java
+++ app/src/main/java/iut/labbeh/simon/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
 
         sm = new SoundManager();
         sm.initSounds(this);
-        sm.addSound(1, R.raw.birds);
+
+        sm.addSound(1, R.raw.aaaah);
+        sm.addSound(2, R.raw.aahhh2);
+        sm.addSound(3, R.raw.aahhzz);
+        sm.addSound(4, R.raw.ah);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN)
+            sm.playSound(1);
+
+        return true;
     }
 }
